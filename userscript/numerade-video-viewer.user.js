43c4deb0c31bb4346b38087cd75fc422ac58eb1c
GooglyBlox <cemcimyildiz@gmail.com>
2024-06-24 19:33:37 -0700

    fix: newly added domains broke userscript

--- userscript/numerade-video-viewer.user.js
+++ userscript/numerade-video-viewer.user.js
@@ -2,7 +2,7 @@
 // @name         Numerade Video Viewer
 // @namespace    https://github.com/GooglyBlox/free-numerade-videos
 // @updateURL    https://raw.githubusercontent.com/GooglyBlox/free-numerade-videos/main/userscript/numerade-video-viewer.user.js
-// @version      1.1
+// @version      1.2
 // @description  Unlock Numerade video answers for free.
 // @author       GooglyBlox
 // @match        https://www.numerade.com/questions/*
@@ -17,9 +17,8 @@
     'use strict';
 
     async function processLink() {
-        const url = window.location.href;
         try {
-            const videoSrc = await fetchVideoSrc(url);
+            const videoSrc = await fetchVideoSrc();
             if (videoSrc) {
                 const videoElement = document.createElement('video');
                 videoElement.src = videoSrc;
@@ -28,12 +27,9 @@
                 videoElement.style.width = '100%';
                 videoElement.style.height = 'auto';
 
-                let containerElement = document.querySelector('.video-redesign__video-container');
-                
-                if (!containerElement) {
-                    containerElement = document.querySelector('.ask-question-detail__simplified-video-container');
-                }
-                
+                let containerElement = document.querySelector('.video-redesign__video-container') ||
+                                       document.querySelector('.ask-question-detail__simplified-video-container');
+
                 if (containerElement) {
                     while (containerElement.firstChild) {
                         containerElement.removeChild(containerElement.firstChild);
@@ -50,58 +46,56 @@
         }
     }
 
-    async function fetchVideoSrc(url) {
+    async function fetchVideoSrc() {
         const doc = document;
-        let videoElement;
-        if (url.startsWith('https://www.numerade.com/ask/question/')) {
-            videoElement = doc.querySelector('img[src^="https://cdn.numerade.com/ask_previews/"]');
-            if (videoElement) {
-                let src = videoElement.getAttribute('src');
-                src = src.replace('_large', '');
-                src = src.substring(0, src.lastIndexOf('.'));
-                const webmSrc = src.replace('ask_previews', 'ask_video') + '.webm';
-                const mp4Src = src.replace('ask_previews', 'ask_video') + '.mp4';
-                return checkVideo(webmSrc, mp4Src);
-            } else {
-                videoElement = doc.querySelector('video[poster^="https://cdn.numerade.com/project-universal/previews/"]');
-                if (videoElement) {
-                    let src = videoElement.getAttribute('poster');
-                    src = src.replace('_large.jpg', '');
-                    const mp4Src = src.replace('previews', 'encoded') + '.mp4';
-                    return mp4Src;
-                }
-            }
-        } else {
-            videoElement = doc.querySelector('img.background-gif');
-            if (videoElement && videoElement.getAttribute('src').endsWith('.gif')) {
-                const webmSrc = videoElement.getAttribute('src').replace('previews', 'encoded').replace('.gif', '.webm');
-                const mp4Src = videoElement.getAttribute('src').replace('previews', 'encoded').replace('.gif', '.mp4');
-                return checkVideo(webmSrc, mp4Src);
-            } else {
-                videoElement = doc.querySelector('video[poster^="https://cdn.numerade.com/project-universal/previews/"]');
-                if (videoElement) {
-                    let src = videoElement.getAttribute('poster');
-                    src = src.replace('_large.jpg', '');
-                    const mp4Src = src.replace('previews', 'encoded') + '.mp4';
-                    return mp4Src;
-                }
-            }
-        }
-        return null;
-    }
+        const possibleSelectors = [
+            'img[src^="https://cdn.numerade.com/ask_previews/"]',
+            'video[poster^="https://cdn.numerade.com/project-universal/previews/"]',
+            'img.background-gif',
+            '.vjs-poster[style*="background-image"]'
+        ];
 
-    async function checkVideo(webmSrc, mp4Src) {
-        const webmResponse = await fetch(webmSrc, { method: 'HEAD' });
-        if (webmResponse.ok) {
-            return webmSrc;
-        } else {
-            const mp4Response = await fetch(mp4Src, { method: 'HEAD' });
-            if (mp4Response.ok) {
-                return mp4Src;
-            } else {
-                return null;
+        for (const selector of possibleSelectors) {
+            const element = doc.querySelector(selector);
+            if (element) {
+                let src;
+                if (selector.includes('style')) {
+                    const style = element.getAttribute('style');
+                    const match = style.match(/url\("(.+?)"\)/);
+                    src = match ? match[1] : null;
+                } else {
+                    src = element.getAttribute(selector.includes('video') ? 'poster' : 'src');
+                }
+
+                if (src) {
+                    src = src.split('?')[0];
+                    const fileExtension = src.split('.').pop().toLowerCase();
+                    src = src.replace(/(_large)?(\.jpg|\.gif|\.png)?$/, '');
+
+                    const baseUrls = [
+                        'https://cdn.numerade.com/ask_previews/',
+                        'https://cdn.numerade.com/project-universal/previews/',
+                        'https://cdn.numerade.com/ask_video/',
+                        'https://cdn.numerade.com/project-universal/encoded/',
+                        'https://cdn.numerade.com/encoded/'
+                    ];
+
+                    const fileTypes = ['webm', 'mp4', 'm4a'];
+
+                    for (const baseUrl of baseUrls) {
+                        for (const fileType of fileTypes) {
+                            const videoSrc = `${baseUrl}${src.split('/').pop()}.${fileType}`;
+                            const response = await fetch(videoSrc, { method: 'HEAD' });
+                            if (response.ok) {
+                                return videoSrc;
+                            }
+                        }
+                    }
+                }
             }
         }
+
+        return null;
     }
 
     processLink();
